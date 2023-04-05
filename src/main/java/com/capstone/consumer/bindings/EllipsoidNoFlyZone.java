7cc6d43179f777748fc7869f8418f86eb0bb0c93
Justin Kaczmarek <justinkaczmarek@unomaha.edu>
2023-04-04 23:06:25 -0500

    test no fly zone controller

---
+++ src/main/java/com/capstone/consumer/bindings/EllipsoidNoFlyZone.java
@@ -0,0 +1,12 @@
+package com.capstone.consumer.bindings;
+
+public class EllipsoidNoFlyZone {
+
+    public String name;
+    public String longitude;
+    public String latitude;
+    public Long altitude;
+    public Long longRadius;
+    public Long latRadius;
+    public Long altRadius;
+}
---
+++ src/main/java/com/capstone/consumer/controllers/AddEllipsoidNoFlyZoneController.java
@@ -0,0 +1,24 @@
+package com.capstone.consumer.controllers;
+
+import com.capstone.consumer.bindings.EllipsoidNoFlyZone;
+import org.apache.kafka.common.protocol.types.Field;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class AddEllipsoidNoFlyZoneController {
+
+    @CrossOrigin
+    @RequestMapping(
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            method = RequestMethod.POST,
+            path = "addNoFlyZone/ellipsoid"
+    )
+    @ResponseBody
+    public String addEllipsoidNoFlyZone(@RequestBody EllipsoidNoFlyZone ellipsoidNoFlyZone){
+        return "add new no fly zone with name=" + ellipsoidNoFlyZone.name;
+    }
+
+}
