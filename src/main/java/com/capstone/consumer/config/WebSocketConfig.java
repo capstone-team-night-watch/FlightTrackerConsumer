ae0a1d76dfff0cad5c8f2cd708a8ed6b54056d03
Derek <dheimes@unomaha.edu>
2023-04-13 19:22:44 -0500

    web socket stuff

--- src/main/java/com/capstone/consumer/config/WebSocketConfig.java
+++ src/main/java/com/capstone/consumer/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.capstone.consumer.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -17,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/consumer-socket").withSockJS();
+        registry.addEndpoint("/consumer-socket").setAllowedOrigins("*").withSockJS();
     }
 }
--- src/main/java/com/capstone/consumer/kafka/KafkaConsumer.java
+++ src/main/java/com/capstone/consumer/kafka/KafkaConsumer.java
@@ -3,19 +3,30 @@ package com.capstone.consumer.kafka;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 @Component
 public class KafkaConsumer {
     Logger logger = Logger.getLogger(String.valueOf(KafkaConsumer.class));
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
     @KafkaListener(id = "kafkaConsumerExampleId", topics="FlightData")
-    @SendTo("/topic/liveCoords")
-    public String listen(String flightData) {
+    public void listen(String flightData) {
         //displayMessageClient.displayMessage(flightData);
         logger.info("Received flightData: " + flightData);
 
-        return flightData;
+        sendMessage(flightData);
+    }
+
+    public void sendMessage(String message) {
+        messagingTemplate.convertAndSend("/topic/liveCoords", message);
     }
 }
