2257029376fe68804c33968f5b0bf5efad6518e4
Derek <dheimes@unomaha.edu>
2023-04-18 19:44:30 -0500

    Chnaged cors pattern

--- src/main/java/com/capstone/consumer/config/WebSocketConfig.java
+++ src/main/java/com/capstone/consumer/config/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/consumer-socket").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/consumer-socket").setAllowedOriginPatterns("*").withSockJS();
     }
 }
