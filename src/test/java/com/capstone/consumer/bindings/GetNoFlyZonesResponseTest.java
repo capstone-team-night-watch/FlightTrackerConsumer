f319d16421ea38f6e4fd670b0468a04749965788
Justin Kaczmarek <justinkaczmarek@unomaha.edu>
2023-04-26 18:09:21 -0500

    fix dependencies

--- src/test/java/com/capstone/consumer/bindings/GetNoFlyZonesResponseTest.java
+++ src/test/java/com/capstone/consumer/bindings/GetNoFlyZonesResponseTest.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(MockitoJUnitRunner.class)
-class GetNoFlyZonesResponseTest {
+public class GetNoFlyZonesResponseTest {
 
     GetNoFlyZonesResponse getNoFlyZonesResponse;
 
