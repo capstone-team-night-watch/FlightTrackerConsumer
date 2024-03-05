e824c2080656f63c220a62a272bf1bf016ea1a4e
Eduardo Montoya <montoyaeduardo2015@gmail.com>
2024-03-05 11:15:12 -0600

    created tests for GetFlightLocationController

--- src/test/java/com/capstone/consumer/controllers/GetFlightLocationControllerTest.java
+++ src/test/java/com/capstone/consumer/controllers/GetFlightLocationControllerTest.java
@@ -1,6 +1,8 @@
 package com.capstone.consumer.controllers;
 
+import com.capstone.consumer.ApplicationProperties;
 import com.capstone.consumer.bindings.GetFlightLocationResponse;
+import com.capstone.consumer.bindings.HealthCheckResponse;
 import com.capstone.consumer.servicehandler.GetFlightLocationServiceHandler;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,13 +11,17 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class GetFlightLocationControllerTest {
 
     @Mock
     private GetFlightLocationServiceHandler serviceHandler;
+
+    @Mock
+    private ApplicationProperties properties;
 
     @InjectMocks
     private GetFlightLocationController getFlightLocationController;
@@ -27,5 +33,21 @@ public class GetFlightLocationControllerTest {
         GetFlightLocationResponse location = getFlightLocationController.getFlightLocation("long", "lat");
 
         assertEquals("Nebraska", location.getLocation());
+    }
+
+    @Test
+    public void returnThingsShouldReturnHealthCheckResponse() {
+        when(properties.getKafkaHostUrl()).thenReturn("kafkaHostUrl");
+        when(properties.getServerPort()).thenReturn("serverPort");
+        HealthCheckResponse healthCheckResponse = getFlightLocationController.returnThings();
+
+        assertNotNull(healthCheckResponse);
+    }
+
+    @Test
+    public void getInNoFlyZoneShouldCallServiceHandler() {
+        double coordinate = 1.0;
+        getFlightLocationController.getInNoFlyZone(coordinate, coordinate, coordinate);
+        verify(serviceHandler, times(1)).handleNoFlyConflict(coordinate, coordinate, coordinate);
     }
 }
