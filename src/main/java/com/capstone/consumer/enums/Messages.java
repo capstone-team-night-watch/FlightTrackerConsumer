2e5bf60d349fdc77aea8fc4fa10f841ea1c21599
Komlan Kodoh <komlankodoh@gmail.com>
2024-03-19 12:48:06 -0600

    Updated socket message

--- src/main/java/com/capstone/consumer/enums/Messages.java
+++ src/main/java/com/capstone/consumer/enums/Messages.java
@@ -6,7 +6,7 @@ public class Messages {
     public static final String NO_FLY_ZONE_CREATED = "no-fly-zone-created";
     public static final String PATH_COLLISION_WITH_NO_FLY_ZONE = "path-collision-with-no-fly-zone";
 
-    public static final String FLIGHT_ENTERED_NO_FLY_ZONE = "path-collision-with-no-fly-zone";
+    public static final String FLIGHT_ENTERED_NO_FLY_ZONE = "flight-entered-no-fly-zone";
 
     public static final String PREDICTED_COLLISION_WITH_NO_FLY_ZONE = "predicted-collision-with-no-fly-zone";
 
